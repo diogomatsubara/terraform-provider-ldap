3a13952eef0835b7001897e6765d7b1f9dec281e
Diogo Matsubara <diogo.matsubara@gmail.com>
2019-09-02 14:09:51 +0200

    Pass cert/key to OpenLDAP container

--- scripts/ldap-up.sh
+++ scripts/ldap-up.sh
@@ -11,6 +11,10 @@ docker run --name my-openldap-container --detach \
     -p 40389:389 \
     -v $(pwd)/.test_env/ldap/ldap:/var/lib/ldap \
     -v $(pwd)/.test_env/ldap/config:/etc/ldap/slapd.d \
+    -v $(pwd)/.test_env/ldap/certs:/container/service/slapd/assets/certs \
+    --env LDAP_TLS_CRT_FILENAME=my-ldap.crt \
+    --env LDAP_TLS_KEY_FILENAME=my-ldap.key \
+    --env LDAP_TLS_CA_CRT_FILENAME=ca.crt \
     osixia/openldap:1.1.9
 
 docker run --name my-phpldapadmin-container --detach \
